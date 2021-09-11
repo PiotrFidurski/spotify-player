b951ce6c4c531f6079717647f386b8e696253dbb
Chimson <crunor55@hotmail.com>
2021-09-11 17:54:21 +0200

    add title and meta tags to HomePage head

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "@styles/home.module.css";
 import type { NextPage } from "next";
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 import * as React from "react";
 
 const Home: NextPage = () => {
@@ -20,10 +21,16 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <TrackProvider>
-        <Sidebar />
-        <Main />
-      </TrackProvider>
+      <Head>
+        <title>Play any Spotify song!</title>
+        <meta name="description" content="Search for a song and play it!" />
+        <TrackProvider>
+          <>
+            <Sidebar />
+            <Main />
+          </>
+        </TrackProvider>
+      </Head>
     </div>
   );
 };
